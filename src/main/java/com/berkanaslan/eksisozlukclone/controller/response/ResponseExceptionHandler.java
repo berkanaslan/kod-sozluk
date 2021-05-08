59ea19c290e4d4fdb86fc4d04df67a63b2a472e2
Berkan Aslan <aslnberkan@gmail.com>
2021-05-08 13:21:05 +0300

    Base things ok.

---
+++ src/main/java/com/berkanaslan/eksisozlukclone/controller/response/ResponseExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.berkanaslan.eksisozlukclone.controller.response;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice()
+public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final String ERROR_MESSAGE = "Error!";
+
+    @ExceptionHandler(value = RuntimeException.class)
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        ResponseWrapper responseWrapper = new ResponseWrapper(ERROR_MESSAGE, ex.getMessage());
+        return handleExceptionInternal(ex, responseWrapper, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+}
---
+++ src/main/java/com/berkanaslan/eksisozlukclone/model/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.berkanaslan.eksisozlukclone.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
