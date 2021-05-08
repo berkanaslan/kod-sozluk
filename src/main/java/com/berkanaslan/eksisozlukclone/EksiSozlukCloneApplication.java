47bd544792006e6960504e9adc65cf08c9dadaf9
Berkan Aslan <aslnberkan@gmail.com>
2021-05-08 12:48:18 +0300

    Maven init.

---
+++ src/main/java/com/berkanaslan/eksisozlukclone/EksiSozlukCloneApplication.java
@@ -0,0 +1,13 @@
+package com.berkanaslan.eksisozlukclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksiSozlukCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EksiSozlukCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/berkanaslan/eksisozlukclone/EksiSozlukCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkanaslan.eksisozlukclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksiSozlukCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
