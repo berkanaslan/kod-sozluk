846fb61190360510a61d7819656fc8fce513e7f6
Berkan Aslan <aslnberkan@gmail.com>
2022-01-01 14:23:07 +0300

    #CloudDB - For details see at description
    application-prod.properties ignored.
    Cloud DB connection completed.
    user table name changed with user_table because of MSSQL constraint.

--- src/main/java/com/berkanaslan/kodsozluk/controller/EntryController.java
+++ src/main/java/com/berkanaslan/kodsozluk/controller/EntryController.java
@@ -1,8 +1,10 @@
 package com.berkanaslan.kodsozluk.controller;
 
 import com.berkanaslan.kodsozluk.model.Entry;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.berkanaslan.kodsozluk.repository.EntryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -25,5 +27,15 @@ public class EntryController extends BaseEntityController<Entry, Entry.Info> {
         return PATH;
     }
 
+    @GetMapping(path = "topic/{topicId}", params = {"pn", "ps", "sb", "sd"})
+    public Page<Entry.Info> getPagedEntriesByTopicId(
+            @PathVariable(name = "topicId") long topicId,
+            @RequestParam(name = "pn", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "ps", defaultValue = "20", required = false) int size,
+            @RequestParam(name = "sb", defaultValue = "id", required = false) String sortBy,
+            @RequestParam(name = "sd", defaultValue = SORT_DIRECTION_ASC, required = false) String sortDirection) {
 
+        final Pageable pageable = preparePageRequest(page, size, sortBy, sortDirection);
+        return ((EntryRepository) getBaseEntityRepository()).findAllByTopicIdOrderByIdAsc(topicId, pageable);
+    }
 }
--- src/main/java/com/berkanaslan/kodsozluk/repository/UserRepository.java
+++ src/main/java/com/berkanaslan/kodsozluk/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.berkanaslan.kodsozluk.repository;
 
 import com.berkanaslan.kodsozluk.model.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends BaseEntityRepository<User> {
     Optional<User> findByUsername(String username);
 
